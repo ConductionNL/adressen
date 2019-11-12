714d59034a418a4881afddf7b497539e6cef4373
Robert Zondervan <rjzondervan@gmail.com>
2019-11-12 09:30:09 +0100

    fixed typo in variable name

--- api/src/Entity/Adres.php
+++ api/src/Entity/Adres.php
@@ -487,7 +487,7 @@ class Adres
 
     public function setStatusWoonplaats(?string $statusWoonplaats): self
     {
-    	$this->statusWoonplaats= $statusWoonplaatse;
+    	$this->statusWoonplaats= $statusWoonplaats;
 
     	return $this;
     }
