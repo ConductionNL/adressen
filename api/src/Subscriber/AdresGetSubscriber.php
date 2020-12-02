e8aa2d06f60b9ef41694f1f904620f9e93de107d
Robert Zondervan <Robert@conduction.nl>
2020-12-02 13:14:19 +0100

    StyleCI dingetje

--- api/src/Subscriber/AdresGetSubscriber.php
+++ api/src/Subscriber/AdresGetSubscriber.php
@@ -13,7 +13,6 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
-use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Serializer\SerializerInterface;
 
