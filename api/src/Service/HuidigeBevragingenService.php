258bdd941fa9361b81d1d9a3c00533b7d2989800
Robert Zondervan <rjzondervan@users.noreply.github.com>
2021-04-26 11:29:30 +0000

    Apply fixes from StyleCI

--- api/src/Service/HuidigeBevragingenService.php
+++ api/src/Service/HuidigeBevragingenService.php
@@ -1,11 +1,8 @@
 <?php
 
-
 namespace App\Service;
 
-
 use App\Entity\Adres;
-use Conduction\CommonGroundBundle\Service\CommonGroundService;
 use Doctrine\ORM\EntityManagerInterface;
 use GuzzleHttp\Client;
 use Symfony\Component\Cache\Adapter\AdapterInterface as CacheInterface;
@@ -14,7 +11,6 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class HuidigeBevragingenService implements KadasterServiceInterface
 {
-
     private Client $client;
     private ParameterBagInterface $params;
     private EntityManagerInterface $entityManager;
@@ -25,21 +21,21 @@ class HuidigeBevragingenService implements KadasterServiceInterface
         $this->params = $params;
         $this->cache = $cache;
         $this->entityManager = $entityManager;
-        $this->client = new Client([
-            // Base URI is used with relative requests
-            'base_uri' => $this->params->get('common_ground.components')['bag']['location'],
-            // You can set any number of default request options.
-            'timeout' => 4000.0,
-            // This api key needs to go into params
-            'headers' => [
-                'X-Api-Key' => $this->params->get('common_ground.components')['bag']['apikey'],
-                'Accept-Crs'  => 'epsg:28992',
+        $this->client = new Client(
+            [
+                // Base URI is used with relative requests
+                'base_uri' => $this->params->get('common_ground.components')['bag']['location'],
+                // You can set any number of default request options.
+                'timeout' => 4000.0,
+                // This api key needs to go into params
+                'headers' => [
+                    'X-Api-Key'   => $this->params->get('common_ground.components')['bag']['apikey'],
+                    'Accept-Crs'  => 'epsg:28992',
                 ],
-            'http_errors' => false,
+                'http_errors' => false,
             ]
         );
     }
-
 
     private function convertQuery($query): string
     {
@@ -64,26 +60,25 @@ class HuidigeBevragingenService implements KadasterServiceInterface
 
     public function getSearchResults(string $search, int $page = 1): array
     {
-
         $item = $this->cache->getItem('searchResults_'.md5($search));
         if ($item->isHit()) {
             return $item->get();
         }
         $results = [];
         $response = $this->client->get('adressen/zoek', ['query' => $this->convertQuery(['zoek' => $search, 'pageSize' => 100, 'page' => $page])]);
-        if($response->getStatusCode() != 200){
+        if ($response->getStatusCode() != 200) {
             throw new HttpException($response->getStatusCode(), $response->getReasonPhrase());
         }
         $response = json_decode($response->getBody()->getContents(), true);
-        if(key_exists('_embedded', $response) && key_exists('zoekresultaten', $response['_embedded'])){
+        if (key_exists('_embedded', $response) && key_exists('zoekresultaten', $response['_embedded'])) {
             $results = $response['_embedded']['zoekresultaten'];
         }
-        if(
+        if (
             key_exists('_links', $response) &&
             key_exists('self', $response['_links']) &&
             key_exists('last', $response['_links']) &&
             $response['_links']['self'] != $response['_links']['last']
-        ){
+        ) {
             $results = array_merge($results, $this->getSearchResults($search, $page + 1));
         }
         $item->set($results);
@@ -100,7 +95,7 @@ class HuidigeBevragingenService implements KadasterServiceInterface
             return $item->get();
         }
         $response = $this->client->get("adresseerbareobjecten/$id");
-        if($response->getStatusCode() != 200){
+        if ($response->getStatusCode() != 200) {
             throw new HttpException($response->getStatusCode(), $response->getReasonPhrase());
         }
         $result = json_decode($response->getBody()->getContents(), true);
@@ -111,11 +106,12 @@ class HuidigeBevragingenService implements KadasterServiceInterface
 
         return $result;
     }
+
     public function getObject(array $addressArray): Adres
     {
-        if(!key_exists('adresseerbaarObjectIdentificatie', $addressArray)){
+        if (!key_exists('adresseerbaarObjectIdentificatie', $addressArray)) {
             var_dump($addressArray);
-            die;
+            exit;
         }
         $addressableObject = $this->getAddressableObject($addressArray['adresseerbaarObjectIdentificatie']);
         $address = new Adres();
@@ -134,7 +130,7 @@ class HuidigeBevragingenService implements KadasterServiceInterface
         if (array_key_exists('straat', $addressArray)) {
             $address->setStraat($addressArray['straat']);
         }
-        if(key_exists('oppervlakte', $addressableObject)){
+        if (key_exists('oppervlakte', $addressableObject)) {
             $address->setOppervlakte($addressableObject['oppervlakte']);
         }
 
@@ -168,7 +164,7 @@ class HuidigeBevragingenService implements KadasterServiceInterface
             return $item->get();
         }
         $response = $this->client->get('adressen', ['query' => $this->convertQuery(['zoekresultaatIdentificatie' => $searchResult['identificatie'], 'expand' => 'openbareRuimte,nummeraanduiding,woonplaats'])]);
-        if($response->getStatusCode() != 200){
+        if ($response->getStatusCode() != 200) {
             throw new HttpException($response->getStatusCode(), $response->getReasonPhrase());
         }
         $response = json_decode($response->getBody()->getContents(), true);
@@ -180,37 +176,40 @@ class HuidigeBevragingenService implements KadasterServiceInterface
         return $response;
     }
 
-    public function getAdressesFromSearchResults (array $searchResults): array
+    public function getAdressesFromSearchResults(array $searchResults): array
     {
         $results = [];
-        foreach($searchResults as $searchResult){
+        foreach ($searchResults as $searchResult) {
             $response = $this->getAddress($searchResult);
 
-            if(key_exists('_embedded', $response) && key_exists('adressen', $response['_embedded'])){
-                foreach($response['_embedded']['adressen'] as $address){
-                    if(!key_exists('nummeraanduidingIdentificatie', $address)){
+            if (key_exists('_embedded', $response) && key_exists('adressen', $response['_embedded'])) {
+                foreach ($response['_embedded']['adressen'] as $address) {
+                    if (!key_exists('nummeraanduidingIdentificatie', $address)) {
                         continue;
                     }
                     $results[] = $this->getObject($address);
                 }
             }
         }
+
         return $results;
     }
 
     public function getAdresOnBagId(string $bagId): Adres
     {
         $response = $this->client->get("adressen/$bagId");
-        if($response->getStatusCode() != 200){
+        if ($response->getStatusCode() != 200) {
             throw new HttpException($response->getStatusCode(), $response->getReasonPhrase());
         }
         $response = json_decode($response->getBody()->getContents(), true);
+
         return $this->getObject($response);
     }
 
     public function getAdresOnHuisnummerPostcode($huisnummer, $postcode): array
     {
         $search = "$huisnummer $postcode";
+
         return $this->getAdressesFromSearchResults($this->getSearchResults($search));
     }
 }
